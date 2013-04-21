a735bd776a865b0f31b7bb29bd30963071f61b9a
Matt Dobson <mdobson4@gmail.com>
2013-04-21 14:35:13 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Maply::Application.config.secret_key_base = 'c7fc5014a7db417ce50d05828a256520c4ee0ce657bc226038c1201a10a18eb49f868added286443d68e85805aa8da28d726d62c9ad9eb301f49ba95fe466ef6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Maply::Application.config.session_store :encrypted_cookie_store, key: '_maply_session'
